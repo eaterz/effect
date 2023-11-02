567e16a54f6d6be4f468b318ac6886bebe523ead
eaterz <muraskoruslans@gmail.com>
2023-11-02 11:34:27 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
 import "./App.css";
 import ToDo from "./ToDo";
+import { useEffect, useState } from "react";
 
 function App() {
-  const todo = {
-    userId: 1,
-    id: 1,
-    title: "delectus aut autem",
-    completed: true,
-  };
+  const [todo, setTodo] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    console.log("hello");
+    async function getData() {
+      const respons = await fetch(
+        "https://jsonplaceholder.typicode.com/todos/130"
+      );
+      const data = await respons.json();
+      console.log(data);
+      setTodo(data);
+      setLoading(false);
+    }
+    getData();
+  }, []);
+
   return (
     <div className="App">
       <h1>Hi</h1>
-      <ToDo {...todo} />
+      {loading ? <p>ladejas.. </p> : <ToDo {...todo} />}
     </div>
   );
 }
